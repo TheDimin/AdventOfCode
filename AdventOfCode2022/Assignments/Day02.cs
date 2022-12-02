30e7f28beef8b341bc9ccf68e863d6d1f7dc785c
Damian <dimin69gaming@outlook.com>
2022-12-02 12:02:28 +0100

    [ADDED] Advent of code 2022 day 2

---
+++ AdventOfCode2022/Assignments/Day02.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using AdventOfCode;
+
+namespace AdventOfCode2021.Assignments
+{
+    class Day02 : DayAssignment
+    {
+
+        struct IVec2
+        {
+            public int forward;
+            public int depth;
+        }
+
+        private IVec2 pos;
+        private int aim = 0;
+        public override int Day { get; } = 2;
+
+        public override void Init()
+        {
+            //numbers = ParseFileToIntArray();
+        }
+
+        public override string A()
+        {
+            pos = new IVec2();
+            foreach (var line in ReadLines())
+            {
+                string[] commands = line.Split(" ");
+                int amount = Convert.ToInt32(commands[1]);
+                switch (commands[0])
+                {
+                    case "forward":
+                        pos.forward += amount;
+                        break;
+                    case "down":
+                        pos.depth += amount;
+                        break;
+                    case "up":
+                        pos.depth -= amount;
+                        break;
+                }
+            }
+            return $"Code: {pos.depth * pos.forward}";
+        }
+
+        public override string B()
+        {
+            pos = new IVec2();
+            foreach (var line in ReadLines())
+            {
+                string[] commands = line.Split(" ");
+                int amount = Convert.ToInt32(commands[1]);
+                switch (commands[0])
+                {
+                    case "forward":
+                        pos.forward += amount;
+                        pos.depth += aim * amount;
+                        break;
+                    case "down":
+                        aim += amount;
+                        break;
+                    case "up":
+                        aim -= amount;
+                        break;
+                }
+            }
+            return  $"Code: {pos.depth * pos.forward}";
+        }
+    }
+}
