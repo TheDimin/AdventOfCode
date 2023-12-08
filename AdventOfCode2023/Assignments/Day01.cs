f6f3ccdc559642564caf8fb8033099a31a898cc3
Damian <dimin69gaming@outlook.com>
2023-12-08 15:59:37 +0100

    Advent of code 2023

---
+++ AdventOfCode2023/Assignments/Day01.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode;
+
+namespace AdventOfCode2021.Assignments
+{
+    class Day01 : DayAssignment
+    {
+
+        public override int Day { get; } = 1;
+
+        public override void Init()
+        {
+            //numbers = ParseFileToIntArray();
+        }
+
+        public override string A()
+        {
+            int sum = 0;
+
+            foreach (var line in ReadLines())
+            {
+                char firsti = ' ';
+                char lasti = ' ';
+                bool first = true;
+                foreach (var lineChar in line)
+                {
+
+                    int i = 0;
+                    if (int.TryParse($"{lineChar}", out i))
+                    {
+                        if (first)
+                        {
+                            firsti = lineChar;
+                            lasti = lineChar;
+                            first = false;
+                        }
+                        else
+                        {
+                            lasti = lineChar;
+                        }
+                    }
+                }
+
+                int ii = 0;
+                int.TryParse($"{firsti}{lasti}", out ii);
+                sum += ii;
+            }
+
+            return $"sum: {sum}";
+        }
+
+        private string[] names = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        public override string B()
+        {
+            int sum = 0;
+
+            foreach (var line in ReadLines())
+            {
+                char firsti = ' ';
+                char lasti = ' ';
+                bool first = true;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    int value = 0;
+                    if (int.TryParse($"{line[i]}", out value))
+                    {
+                        if (first)
+                        {
+                            firsti = line[i];
+                            lasti = line[i];
+                            first = false;
+                        }
+                        else
+                        {
+                            lasti = line[i];
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 0; j < 9; j++)
+                        {
+                            if (line.Substring(i).StartsWith(names[j]))
+                            {
+                                if (first)
+                                {
+                                    firsti = $"{j + 1}"[0];
+                                    lasti = firsti;
+                                    first = false;
+                                }
+                                else
+                                {
+                                    lasti = $"{j + 1}"[0];
+                                }
+
+                               // i += names[j].Length - 1;
+
+                                break;
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine($"{firsti}{lasti}");
+                int ii = 0;
+                int.TryParse($"{firsti}{lasti}", out ii);
+                sum += ii;
+            }
+
+            return $"sum: {sum}";
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,201 @@
+﻿#define DEVELOP
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using AdventOfCode;
+
+
+namespace AdventOfCode2021
+{
+
+    class LogThrow : Exception
+    {
+        public LogThrow(string message) : base(message)
+        {
+        }
+    }
+    static class Application
+    {
+        private static Dictionary<int, DayAssignment> assignments = new Dictionary<int, DayAssignment>();
+
+        static void Main(string[] args)
+        {
+            foreach (Type type in Assembly.GetAssembly(typeof(DayAssignment)).GetTypes().Where(classType =>
+                classType.IsClass && !classType.IsAbstract && classType.IsSubclassOf(typeof(DayAssignment))))
+            {
+                var assignment = (DayAssignment)Activator.CreateInstance(type);
+                assignments.Add(assignment.Day, assignment);
+            }
+            int targetDay = 1;
+            while (true)
+            {
+
+                
+#if !DEVELOP
+                if (args.Length == 0)
+                {
+                    bool invalid;
+
+                    do
+                    {
+                        Console.Clear();
+                        Console.Write("Load day: ");
+                        string line = Console.ReadLine();
+                        invalid = !int.TryParse(line, out targetDay);
+                        if (!invalid)
+                            invalid = !assignments.ContainsKey(targetDay);
+                        else if (line == "perf")
+                        {
+                            PerformancePrint();
+                        }
+
+                    } while (invalid);
+                }
+                else
+                {
+                    targetDay = int.Parse(args[0]);
+                }
+#endif
+                Console.Clear();
+                Stopwatch timer = new Stopwatch();
+                TimeSpan InitTime;
+                TimeSpan ATime;
+                TimeSpan BTime;
+
+                try
+                {
+                    timer.Start();
+                    assignments[targetDay].Init();
+                }
+                catch (NotImplementedException)
+                {
+                    Console.WriteLine("Not Implemented");
+                }
+
+                InitTime = timer.Elapsed;
+
+                Console.WriteLine($"Day: {targetDay} \n\n");
+                Console.WriteLine("Assignment A: ");
+
+                try
+                {
+                    timer.Restart();
+                    Console.WriteLine(assignments[targetDay].A());
+                }
+                catch (NotImplementedException)
+                {
+                    Console.WriteLine("Not Implemented");
+                }
+
+                ATime = timer.Elapsed;
+
+                Console.WriteLine("");
+                Console.WriteLine("=========================");
+                Console.WriteLine("");
+                Console.WriteLine("Assignment B: ");
+                try
+                {
+                    timer.Restart();
+                    Console.WriteLine(assignments[targetDay].B());
+                }
+                catch (NotImplementedException)
+                {
+                    Console.WriteLine("Not Implemented");
+                }
+                catch (LogThrow e)
+                {
+                    Console.Clear();
+                    Console.Write(e.Message);
+
+                    Console.WriteLine("Press any key to return");
+                    Console.ReadKey();
+                    continue;
+
+                }
+
+                BTime = timer.Elapsed;
+                timer.Stop();
+
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine($"Init executionTime: {InitTime.TotalMilliseconds} MS");
+                Console.WriteLine($"A executionTime: {ATime.TotalMilliseconds} MS");
+                Console.WriteLine($"B executionTime: {BTime.TotalMilliseconds} MS");
+                Console.WriteLine($"Total executionTime: {(InitTime + ATime + BTime).TotalMilliseconds} MS");
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("Press any key to return");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void PerformancePrint()
+        {
+            Console.Clear();
+            Console.WriteLine("Starting Performance Run");
+            Stopwatch timer = new Stopwatch();
+            TimeSpan total = new TimeSpan();
+            foreach (var dayPair in assignments.OrderBy(t => t.Key))
+            {
+                TimeSpan assignmentTimeSpan = new TimeSpan();
+                DayAssignment assignment = dayPair.Value;
+
+                Console.WriteLine($"Day: {dayPair.Key}");
+                try
+                {
+                    timer.Restart();
+                    assignment.Init();
+
+                }
+                catch (NotImplementedException)
+                {
+                    Console.WriteLine("Not Implemented");
+                }
+                timer.Stop();
+                Console.WriteLine($"Init: {timer.Elapsed.TotalMilliseconds} MS");
+                assignmentTimeSpan += timer.Elapsed;
+
+                try
+                {
+                    timer.Restart();
+                    assignment.A();
+                }
+                catch (NotImplementedException)
+                {
+                    Console.WriteLine("Not Implemented");
+                }
+
+                timer.Stop();
+                Console.WriteLine($"A: {timer.Elapsed.TotalMilliseconds} MS");
+                assignmentTimeSpan += timer.Elapsed;
+                try
+                {
+                    timer.Restart();
+                    assignment.B();
+
+                }
+                catch (NotImplementedException)
+                {
+                    Console.WriteLine("Not Implemented");
+                }
+
+                timer.Stop();
+                Console.WriteLine($"B: {timer.Elapsed.TotalMilliseconds} MS");
+                assignmentTimeSpan += timer.Elapsed;
+
+                Console.WriteLine($"Total: {assignmentTimeSpan.TotalMilliseconds} MS");
+                total += assignmentTimeSpan;
+                Console.WriteLine("\n=======================\n\n");
+            }
+            Console.WriteLine($"Total Execution time {total.TotalMilliseconds} MS");
+
+            Console.WriteLine("Press any key to return");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
